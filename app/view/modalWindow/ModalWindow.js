27f19f6ccd15407da648c05a40e530898e4e961d
stanislau vasilkou <stanlymia98@gmail.com>
2019-11-01 11:59:44 +0300

    applied some styles to modal window

--- app/view/modalWindow/ModalWindow.js
+++ app/view/modalWindow/ModalWindow.js
@@ -22,103 +22,103 @@ const skills = Ext.create('Ext.data.Store', {
 
 Ext.define('MyApp.view.modalWindow.ModalWindow', {
 	extend: 'Ext.window.Window',
+	minWidth: 400,
 	title: 'Modal window',
 	xtype: 'modal',
-
-	requires: [
-		'Ext.form.Panel'
-	],
 	controller: 'modalWindow',
-	// width: 600,
-	// height: 400,
 	modal: true,
 	padding: 10,
-	bbarCfg: {
-		buttonAlign: 'center'
-	},
-	items: {
-		xtype: 'form',
-		id: 'customForm',
-		items: [
-			{
-				xtype: 'textfield',
-				fieldLabel: 'name',
-				name: 'name',
-				flex: 1,
-				allowBlank: false,
-				bind: {
-					value: '{user.name}'
-				}
-			}, {
-				xtype: 'combobox',
-				fieldLabel: 'region',
-				name: 'region',
-				valueField: 'name',
-				displayField: 'name',
-				queryMode: 'local',
-				store: regions,
-				allowBlank: false,
-				bind: {
-					value: '{user.region}'
-				}
-			}, {
-				xtype: 'tagfield',
-				fieldLabel: 'skills',
-				name: 'skills',
-				anchor: '100%',
-				displayField: 'skill',
-				valueField: 'skill',
-				store: skills,
-				allowBlank: false,
-				bind: {
-					value: '{user.skills}'
-				},
-				convert: function () {
-
-				}
-			}, {
-				xtype: 'numberfield',
-				fieldLabel: 'salary',
-				name: 'salary',
-				hideTrigger: true,
-				format: '0',
-				allowBlank: false,
-				bind: {
-					value: '{user.salary}'
-				}
-			}, {
-				xtype: 'container',
-				layout: {
-					type: 'hbox',
-				},
-				items: [{
-					xtype: 'datefield',
-					name: 'datecolumn',
-					fieldLabel: 'date',
-					format: 'Y-m-d',
+	items: [
+		{
+			xtype: 'form',
+			id: 'customForm',
+			items: [
+				{
+					xtype: 'textfield',
+					fieldLabel: 'name',
+					name: 'name',
 					allowBlank: false,
 					bind: {
-						value: '{user.datecolumn}'
+						value: '{user.name}'
 					}
 				}, {
-					xtype: 'timefield',
-					name: 'timecolumn',
-					fieldLabel: 'time',
-					format: 'g:i:s',
-					allowBlank: false
+					xtype: 'combobox',
+					fieldLabel: 'region',
+					name: 'region',
+					valueField: 'name',
+					displayField: 'name',
+					queryMode: 'local',
+					store: regions,
+					allowBlank: false,
+					bind: {
+						value: '{user.region}'
+					}
+				}, {
+					xtype: 'tagfield',
+					fieldLabel: 'skills',
+					name: 'skills',
+					anchor: '100%',
+					displayField: 'skill',
+					valueField: 'skill',
+					store: skills,
+					allowBlank: false,
+					bind: {
+						value: '{user.skills}'
+					},
+				}, {
+					xtype: 'numberfield',
+					fieldLabel: 'salary',
+					name: 'salary',
+					hideTrigger: true,
+					format: '0',
+					allowBlank: false,
+					bind: {
+						value: '{user.salary}'
+					}
+				}, {
+					xtype: 'container',
+					layout: 'hbox',
+					items: [
+						{
+							xtype: 'datefield',
+							fieldLabel: 'date',
+							name: 'datecolumn',
+							format: 'Y-m-d',
+							allowBlank: false,
+							bind: {
+								value: '{user.datecolumn}'
+							}
+						}, {
+							xtype: 'timefield',
+							fieldLabel: 'time',
+							name: 'timecolumn',
+							padding: '0 0 0 10',
+							format: 'g:i:s',
+							allowBlank: false
+						}]
 				}]
-			}
-		],
-	},
-	bbar: [
-		{
-			xtype: 'button',
-			text: 'Сохранить',
-			handler: 'saveData'
 		}, {
-			xtype: 'button',
-			text: 'Закрыть',
-			handler: 'close'
-		}
-	]
+			xtype: 'container',
+			minWidth: 600,
+			margin: '10 0 0 0',
+			id: 'cont',
+			items: [
+				{
+					xtype: 'button',
+					width: 100,
+					text: 'Сохранить',
+					handler: 'saveData'
+				}, {
+					xtype: 'button',
+					width: 100,
+					text: 'Закрыть',
+					handler: 'close'
+				}
+			],
+			layout: {
+				type: 'hbox',
+				pack: 'end',
+				align: 'middle'
+			},
+		}]
 });
